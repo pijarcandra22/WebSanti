4f69df6258ba5b1e5d7b5725a9623fcb086811a1
santicahyani <santicahyani51@gmail.com>
2023-02-28 18:46:55 +0800

    push fiks

--- app.py
+++ app.py
@@ -14,6 +14,13 @@ scaler.fit(pd.read_csv("DatasetTugasAkhir.csv").iloc[:, :-1].to_numpy())
 model = pickle.load(open("model2.sav", 'rb'))
 
 
+def change(x):
+    if x == 0:
+        return 1
+    else:
+        return 0
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -31,7 +38,9 @@ def inputdata():
 
 @app.route('/patientdata')
 def patientdata():
-    return render_template("patientdata.html")
+    hasil = pd.read_csv("dataHasil.csv")
+    hasil['Risiko'] = hasil['Risiko'].apply(change)
+    return render_template("patientdata.html", hasil=hasil)
 
 
 @app.route('/riskresult')
@@ -45,11 +54,19 @@ def fknn():
     print(data['age'])
 
     data = pd.DataFrame.from_dict(data)
+    backup = data.copy()
     dt = scaler.transform(data.values)
     data = pd.DataFrame(dt, columns=data.columns)
 
     hasil = model.predict(data)
     print(hasil)
+    backup["Risiko"] = np.array(hasil[0])
+    backup.columns = ['Umur', 'Tinggi' 'Badan', 'BB',
+                      'LILA', 'sistolik', 'diastolik', 'Risiko']
+
+    backup = pd.concat([backup, pd.read_csv("dataHasil.csv")], axis=0)
+    backup.to_csv("dataHasil.csv", index=False)
+
     return str(hasil[0])
 
 
